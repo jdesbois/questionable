aa33fee22290d0f54911c710552032463045e934
Andrew Allan <andrew.allan1@outlook.com>
2020-03-05 18:42:22 +0000

    Generating index page including content from view

--- main/views.py
+++ main/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Http response working")
+
+    context_dict = {'message': 'Message sent from the view'}
+
+    return render(request, 'main/index.html', context=context_dict)
