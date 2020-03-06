73f1ecacbd3233b1a6e71606b8669c3ca58b05de
Andrew Allan <andrew.allan1@outlook.com>
2020-03-06 12:18:08 +0000

    Added course and reply models

--- main/models.py
+++ main/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 
-class Lecture(models.Model):
+class Course(models.Model):
     name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Lecture(models.Model):
+    # why doesn't this work without setting default?
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, default=None)
+    name = models.CharField(max_length=128)
 
     def __str__(self):
         return self.name
@@ -16,3 +25,12 @@ class Question(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Reply(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    reply = models.CharField(max_length=512)
+
+    def __str__(self):
+        # identify by primary key
+        return "Reply: " + self.pk
