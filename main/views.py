6477be8143e2a2dfa89749fcbd90a33a7e344d72
John Desbois <jdesbois@me.com>
2020-03-06 22:17:18 +0000

    added static and media dir settings

--- main/views.py
+++ main/views.py
@@ -5,4 +5,4 @@ def index(request):
 
     context_dict = {'message': 'Message sent from the view'}
 
-    return render(request, 'main/index.html', context=context_dict)
+    return render(request, 'main/index-mainpage.html', context=context_dict)
