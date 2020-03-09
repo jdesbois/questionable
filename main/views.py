169b84f9c37039175896f5c6a7a8d6f3d551d2be
Aaron Callaghan <aaroncallaghan40@gmail.com>
2020-03-09 12:52:05 +0000

    Basic Form objects and set up View methods

--- main/views.py
+++ main/views.py
@@ -2,35 +2,49 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-
-
 def index(request):
-
     context_dict = {'message': 'Message sent from the view'}
     return render(request, 'main/index-mainpage.html', context=context_dict)
 
 
-def userProfilePage(request):
-    
+def user_profile_page(request):
     return render(request)
 
-def courseList(request):
-    
-    return
-    
-def questionsPage(request):
-    
-    return 
 
-def login(request):
-    
+def course_list(request):
+
+    context_dict = {}
+
     return
 
-def contactPage(request):
-    
+
+def questions_page(request):
     return
+
+
+def user_login(request):
+    return
+
+
+def contact_page(request):
+    return
+
+
+def create_course(request):
+    return
+
+
+def create_lecture(request):
+    return
+
+
+def create_reply(request):
+    return
+
+
+def create_comment(request):
+    return
+
 
 def profile(request):
-    
     return render(request, 'registration/profile.html')
-
