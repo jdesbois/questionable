55a0b0cd608e2179ea48780b1205de1e1742e19c
John Desbois <jdesbois@me.com>
2020-03-11 20:11:48 +0000

    added a basic test to see if they run

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from main.models import Course
 
 # Create your tests here.
+class CourseTestCase(TestCase):
+    def setUp(self):
+        Course.objects.create(name="Programming")
+        Course.objects.create(name="Database")
+
+    def test_course_name(self):
+        course1 = Course.objects.get(name="Programming")
+        course2 = Course.objects.get(name="Database")
+        self.assertEquals(course1.name, "Programming")
+        self.assertEquals(course2.name, "Database")
