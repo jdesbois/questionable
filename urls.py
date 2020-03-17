1a4326717052981fd2e023dbaf7018cfe59ea728
rdinneen <rebecca.dinneen@gmail.com>
2020-03-17 13:05:02 +0000

    Base template created

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
-"""questionable URL Configuration
+"""Questionable URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,14 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from main import views
-
-app_name = 'questionable'
+from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('main/', include('main.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('registration.backends.simple.urls')),
 ]
