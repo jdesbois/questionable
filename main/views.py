2db2e3e5e62900ff431b97838d61f58939c804f1
Aaron Callaghan <aaroncallaghan40@gmail.com>
2020-03-18 16:48:50 +0000

    Updated view to run new templates and created create_course and create_lecture templates

--- main/views.py
+++ main/views.py
@@ -51,7 +51,7 @@ def show_course(request, course_name_slug):
         context_dict['course'] = None
         context_dict['lectures'] = None
         context_dict['forums'] = None
-    return render(request, 'main/course.html', context=context_dict)
+    return render(request, 'main/course_details.html', context=context_dict)
 
 
 # @login_required
@@ -209,11 +209,20 @@ def create_course(request):
 
             print(form.errors)
 
-    return render(request, 'main/courses/', {'form': form})
+    return render(request, 'main/create_course.html', {'form': form})
 
 
 @login_required
-def create_lecture(request):
+def create_lecture(request, course_name_slug):
+
+    try:
+        course = Course.objects.get(slug=course_name_slug)
+    except Course.DoesNotExist:
+        course = None
+
+    if course is None:
+        return redirect('/main/course/<slug:course_name_slug>/')
+
     form = LectureForm()
 
     # If user inputs comment
@@ -222,19 +231,22 @@ def create_lecture(request):
         # If input is valid
         if form.is_valid():
             # Save the form
-            lecture = form.save(commit=True)
-            lecture.course = request.course
+            lecture = form.save(commit=False)
+            lecture.course = course
+            lecture.save()
 
-            return redirect('/main/course/<slug:course_name_slug>/')
+            return redirect(reverse('main:course',
+                                    kwargs={'course_name_slug': course_name_slug}))
 
         else:
 
             print(form.errors)
 
-    return render(request, 'main/course/<slug:course_name_slug>/create_lecture.html', {'form': form})
+    context_dict = {'form': form, 'course': course}
+    return render(request, 'main/create_lecture.html', context_dict)
 
 
-# @login_required
+@login_required
 def create_question(request, course_name_slug, lecture_name_slug):
 
     # find lecture object
@@ -242,6 +254,14 @@ def create_question(request, course_name_slug, lecture_name_slug):
         lecture = Lecture.objects.get(slug=lecture_name_slug)
     except Lecture.DoesNoExist:
         lecture = None
+
+    try:
+        course = Course.objects.get(slug=course_name_slug)
+    except Course.DoesNotExist:
+        course = None
+
+    if course is None or lecture is None:
+        return redirect('/main/course/<slug:course_name_slug>/<slug:lecture_name_slug/')
 
     form = QuestionForm()
 
