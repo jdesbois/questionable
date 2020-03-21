f6f554ef5745fb7e419aa1749582b38d399e7ba3
Andrew Allan <andrew.allan1@outlook.com>
2020-03-21 16:10:43 +0000

    Assigned users to content, added replies and comments

--- populate_main.py
+++ populate_main.py
@@ -14,32 +14,163 @@ from django.contrib.auth import get_user_model
 
 
 def populate():
+    ##############
+    # Groups
+    ##############
+    lecturer = Group(name="Lecturer")
+    # lecturer = Group.objects.create(name='Lecturer')
+    lecturer.save()
+    student = Group(name="Student")
+    # student = Group.objects.create(name='Student')
+    student.save()
+    # extract perms of model object and adds it to specified group
+    lecture_perms = ContentType.objects.get(app_label='main', model='lecture')
+    l_perms = Permission.objects.filter(content_type=lecture_perms)
+
+    reply_perms = ContentType.objects.get(app_label='main', model='reply')
+    r_perms = Permission.objects.filter(content_type=reply_perms)
+
+    comment_perms = ContentType.objects.get(app_label='main', model='comment')
+    com_perms = Permission.objects.filter(content_type=comment_perms)
+
+    course_perms = ContentType.objects.get(app_label='main', model='course')
+    c_perms = Permission.objects.filter(content_type=course_perms)
+
+    forum_perms = ContentType.objects.get(app_label='main', model='forum')
+    f_perms = Permission.objects.filter(content_type=forum_perms)
+
+    for x in l_perms:
+        lecturer.permissions.add(x)
+    for x in r_perms:
+        lecturer.permissions.add(x)
+    for x in com_perms:
+        student.permissions.add(x)
+    for x in c_perms:
+        lecturer.permissions.add(x)
+    for x in f_perms:
+        lecturer.permissions.add(x)
+
+    ##############
+    # Users
+    ##############
+
+    users = [
+        {
+            'username': 'Dory',
+            'email': 'noreply@apple.com',
+            'password': 'dorypassword1',
+        },
+        {
+            'username': 'Mike',
+            'email': 'noreply@apple.com',
+            'password': 'mikepassword1',
+        },
+        {
+            'username': 'Becca',
+            'email': 'noreply@apple.com',
+            'password': 'beccapassword1',
+        },
+        {
+            'username': 'Archie',
+            'email': 'noreply@apple.com',
+            'password': 'archiepassword1',
+        },
+        {
+            'username': 'Alan',
+            'email': 'noreply@apple.com',
+            'password': 'alanpassword1',
+        },
+        {
+            'username': 'Claire',
+            'email': 'noreply@apple.com',
+            'password': 'clairepassword1',
+        },
+        {
+            'username': 'Dave',
+            'email': 'noreply@apple.com',
+            'password': 'davepassword1',
+        },
+        {
+            'username': 'Indy',
+            'email': 'indy@carrot.com',
+            'password': 'ILoveCarrots',
+        },
+    ]
+
+    for user in users:
+        u = User.objects.create_user(user['username'], user['email'], user['password'])
+        add_student(u)
+
+    # Default admin for /admin (REMOVE BEFORE DEPLOYING)
+    admin = User.objects.create_user('admin', 'noreply@apple.com', 'HelloWorld123')
+    User1 = get_user_model()
+    user = User1.objects.get(username="admin")
+    user.is_staff = True
+    user.is_admin = True
+    user.is_superuser = True
+    user.save()
+
+    user1 = User.objects.create_user("John", "noreplay@apple.com", "johnpassword1")
+    user2 = User.objects.create_user("Andrew", "noreplay@apple.com", "andrewpassword1")
+    user3 = User.objects.create_user("Rebecca", "noreplay@apple.com", "rebeccapassword1")
+    user4 = User.objects.create_user("Aaron", "noreplay@apple.com", "aaronpassword1")
+    user5 = User.objects.create_user("Bob", "noreplay@apple.com", "bobpassword1")
+
+    student3 = add_student(user3)
+    student4 = add_student(user4)
+
+    user4.groups.add(student)
+    user3.groups.add(student)
+    tutor1 = add_tutor(user1)
+    tutor2 = add_tutor(user2)
+
+    user1.groups.add(lecturer)
+    user2.groups.add(lecturer)
 
     ####################
     # Questions
     ####################
 
     hello_world_questions = [
-        {'title': 'Help, I\'ve deleted my path variable.',
-         'question': 'Where can I buy a new laptop?'},
-        {'title': 'What is MVC?',
-        'question': 'Unsure how to implement model, view controller.'}
+        {'title': 'What version of Java are we using?',
+         'question': "I've seen online that there are 13 versions of Java, which version will we use for this course?",
+         'author': student3,
+         'replies': [["We will be using Java 1.8.", tutor1],
+                     ["Also worth investigating Eclipse, the development environment the staff will use to teach.",
+                      tutor1]]},
+        {'title': 'Main method help.',
+         'question': 'What is the correct way to write a main metho?',
+         'author': student4,
+         'replies': [["public static void main(String args){}", tutor1],
+                     ["Try inserting 'System.out.println(\"Hello World\");' in this method.", tutor1]]}
     ]
 
-    intro_to_java_questions =[]
+    intro_to_java_questions = []
 
     objects_questions = [
         {'title': 'Constructors',
-         'question': 'How is a constructor created and used?'},
+         'question': 'How is a constructor created and used?',
+         'author': student4,
+         'replies': [["A constructor is called to create an initial instance of an object." +
+                      "It can be passed variables and use these to set the initial parameters of the object",
+                      tutor2]]},
         {'title': 'Pass by reference',
-         'question': 'What is the difference between pass by reference and by value?'}
+         'question': 'What is the difference between pass by reference and by value?',
+         'author': student3,
+         'replies': [["When an object is passed by reference, the passed value is an address pointing to that object." +
+                      "When an object is passed by value the object itself is passed", tutor2],
+                     ["Note that objects can only be passed by reference in Java.", tutor2]]}
     ]
 
     DBIntro_questions = [
         {'title': 'What is a database',
-         'question': 'And how does it work?'},
+         'question': 'And how does it work?',
+         'author': student4,
+         'replies': []},
         {'title': 'What is a foreign key?',
-         'question': 'And where is it from?'}
+         'question': 'And where is it from?',
+         'author': student3,
+         'replies': []}
     ]
 
     ERDiagram_questions = [
@@ -48,9 +179,13 @@ def populate():
 
     SQL_questions = [
         {'title': 'My database is odd',
-         'question': 'How can I normalize it?'},
+         'question': 'How can I normalize it?',
+         'author': student3,
+         'replies': []},
         {'title': 'SQL vs NoSQL',
-         'question': 'What is the difference between SQL and NoSQL?'}
+         'question': 'What is the difference between SQL and NoSQL?',
+         'author': student4,
+         'replies': []}
     ]
 
     ################
@@ -65,11 +200,9 @@ def populate():
                          'ER Diagrams': {'questions': ERDiagram_questions},
                          'SQL': {'questions': SQL_questions}}
 
-
     ####################
     # Comments (Post)
     ####################
-
 
     ####################
     # Posts (Forum)
@@ -77,18 +210,28 @@ def populate():
 
     programming_forum_posts = [
         {'title': 'Book for sale - Big Java Late Objects',
-            'post': 'Book for sale, unused.'},
+         'post': 'Book for sale, unused.',
+         'author': user3,
+         'comments': [["How much?", user4],
+                      ["£10", user3],
+                      ["Can I pick it up Friday?", user4]]},
         {'title': 'Group wanted.',
-            'post': 'Looking for group members for project.'}
+         'post': 'Looking for group members for project.',
+         'author': user4,
+         'comments': [["We need one more for our team if you'd like to join?", user3],
+                      ["That would be great, thanks!", user4]]}
     ]
 
     database_forum_posts = [
         {'title': 'Anyone else having trouble with Postgres?',
-         'post': 'Server won\'t run.'},
+         'post': 'Server won\'t run.',
+         'author': user4,
+         'comments': [["I did, I found the lecture video from 23/10 helpful.", user3]]},
         {'title': 'Study buddies wanted - apply within.',
-         'post': 'Looking for people to study databases with'}
+         'post': 'Looking for people to study databases with',
+         'author': user3,
+         'comments': [["We have a study group every Thursday at 7pm", user4]]}
     ]
-
 
     ################
     # Forums
@@ -105,136 +248,20 @@ def populate():
     courses = {'Programming': {'lectures': programming_lectures, 'forum': programming_forum},
                'Databases': {'lectures': database_lectures, 'forum': database_forum}}
 
-    ##############
-    # Groups
-    ##############
-    lecturer = Group(name="Lecturer")
-    # lecturer = Group.objects.create(name='Lecturer')
-    lecturer.save()
-    student = Group(name="Student")
-    # student = Group.objects.create(name='Student')
-    student.save()
-    #extract perms of model object and adds it to specified group
-    lecture_perms = ContentType.objects.get(app_label='main', model='lecture')
-    l_perms = Permission.objects.filter(content_type=lecture_perms)
-
-    reply_perms = ContentType.objects.get(app_label='main', model='reply')
-    r_perms = Permission.objects.filter(content_type=reply_perms)
-
-    comment_perms = ContentType.objects.get(app_label='main', model='comment')
-    com_perms = Permission.objects.filter(content_type=comment_perms)
-
-    course_perms = ContentType.objects.get(app_label='main', model='course')
-    c_perms = Permission.objects.filter(content_type=course_perms)
-
-    forum_perms = ContentType.objects.get(app_label='main', model='forum')
-    f_perms = Permission.objects.filter(content_type=forum_perms)
-    
-    for x in l_perms:
-        lecturer.permissions.add(x)
-    for x in r_perms:
-        lecturer.permissions.add(x) 
-    for x in com_perms:
-        student.permissions.add(x)
-    for x in c_perms:
-        lecturer.permissions.add(x)
-    for x in f_perms:
-        lecturer.permissions.add(x)
-
-
-
-    ##############
-    # Users
-    ##############
-
-    users = [
-        {
-            'username' : 'Dory',
-            'email': 'noreply@apple.com',
-            'password': 'dorypassword1',
-        },
-        {
-            'username' : 'Mike',
-            'email': 'noreply@apple.com',
-            'password': 'mikepassword1',
-        },
-        {
-            'username' : 'Becca',
-            'email': 'noreply@apple.com',
-            'password': 'beccapassword1',
-        },
-        {
-            'username' : 'Archie',
-            'email': 'noreply@apple.com',
-            'password': 'archiepassword1',
-        },
-        {
-            'username' : 'Alan',
-            'email': 'noreply@apple.com',
-            'password': 'alanpassword1',
-        },
-        {
-            'username' : 'Claire',
-            'email': 'noreply@apple.com',
-            'password': 'clairepassword1',
-        },
-        {
-            'username' : 'Dave',
-            'email': 'noreply@apple.com',
-            'password': 'davepassword1',
-        },
-        {
-            'username' : 'Indy',
-            'email': 'indy@carrot.com',
-            'password': 'ILoveCarrots',
-        },
-    ]
-
-    for user in users:
-       u =  User.objects.create_user(user['username'], user['email'], user['password'])
-       add_student(u)
-        
-  
-
-    # Default admin for /admin (REMOVE BEFORE DEPLOYING)
-    admin = User.objects.create_user('admin', 'noreply@apple.com','HelloWorld123')
-    User1 = get_user_model()
-    user = User1.objects.get(username="admin")
-    user.is_staff = True
-    user.is_admin = True
-    user.is_superuser = True
-    user.save()
-
-    user1 = User.objects.create_user("John", "noreplay@apple.com", "johnpassword1")
-    user2 = User.objects.create_user("Andrew", "noreplay@apple.com", "andrewpassword1")
-    user3 = User.objects.create_user("Rebecca", "noreplay@apple.com", "rebeccapassword1")
-    user4 = User.objects.create_user("Aaron", "noreplay@apple.com", "aaronpassword1")
-    user5 = User.objects.create_user("Bob", "noreplay@apple.com", "bobpassword1")
-
-
-    student3 = add_student(user3)
-    student4 = add_student(user4)
-    
-    user4.groups.add(student)
-    user3.groups.add(student)
-    tutor1 = add_tutor(user1)
-    tutor2 = add_tutor(user2)
-
-    user1.groups.add(lecturer)
-    user2.groups.add(lecturer)
-
     for course, course_data in courses.items():
         c = add_course(course, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse laoreet consectetur odio ut fermentum. Mauris eleifend facilisis placerat. Praesent nec velit consequat, suscipit dui quis, maximus tellus. Donec volutpat consectetur ex a ultrices. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean ullamcorper tempus egestas. In auctor a risus consectetur cursus. Phasellus in sodales nibh. Duis finibus diam lectus, et pellentesque massa feugiat at. Donec molestie rutrum varius. In sollicitudin, massa id tristique rhoncus, odio risus consectetur quam, vel iaculis neque nisi non arcu. Suspendisse vulputate dolor nulla, vel tristique purus pretium ut. In hac habitasse.")
         for lecture, lecture_data in course_data['lectures'].items():
             l = add_lecture(c, lecture)
             for question in lecture_data['questions']:
-                q = add_question(l, question['title'], question['question'], student4)
-                add_reply(q, "Donec aliquam dolor sapien, sagittis posuere dolor molestie vel. Aliquam arcu orci, luctus id vestibulum eget, dignissim egestas leo. Vivamus bibendum augue augue, a gravida ante condimentum id. Quisque ut rhoncus nulla. Ut eleifend est ut dui ultrices interdum. Quisque nec vulputate felis. Ut non tortor turpis. Vestibulum pretium nec erat vitae finibus. Maecenas consequat est sit amet fringilla fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;", tutor1)
+                q = add_question(l, question['title'], question['question'], question['author'])
+                for reply in question['replies']:
+                    add_reply(q, reply[0], reply[1])
         for forum, forum_data in course_data['forum'].items():
             f = add_forum(c, forum)
             for post in forum_data['posts']:
-                p = add_post(f, post['title'], post['post'])
-                add_comment(p, "Test reply text", user5)
+                p = add_post(f, post['title'], post['post'], post['author'])
+                for comment in post['comments']:
+                    add_comment(p, comment[0], comment[1])
 
 
 def add_reply(question, reply, user):
@@ -265,9 +292,10 @@ def add_comment(post, comment, user):
     return c
 
 
-def add_post(forum, title, post):
+def add_post(forum, title, post, user):
     q = Post.objects.get_or_create(forum=forum, title=title)[0]
     q.post = post
+    q.user = user
     q.save()
     return q
 
